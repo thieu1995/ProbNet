528eb63d9f2ced3f3261f9899c20ae3824e40fd3
anh9895 <honganh090895@gmail.com>
2025-05-03 16:10:59 +0700

    Update tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 16:09, 03/05/2025 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+
+
